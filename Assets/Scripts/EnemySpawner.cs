8ea60c2725d0a0ce03955dcde403ca4955f4ea28
PierceNunn <agentp5151@gmail.com>
2024-08-31 22:39:09 -0500

    created enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _spawnPoints;
+    [SerializeField] private GameObject[] _enemyTypes;
+    [SerializeField] private float _spawnTime;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            GameObject e = _enemyTypes[Random.Range(0, _enemyTypes.Length)];
+            Vector3 spawnloc = _spawnPoints[Random.Range(0,
+                _spawnPoints.Length)].transform.position;
+
+            Instantiate(e, spawnloc, Quaternion.identity);
+
+            yield return new WaitForSeconds(_spawnTime);
+        }
+        
+        
+        
+    }
+}
